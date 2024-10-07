c9dffd8f6c543a997b393e2990d0c621dad24b80
hensol1 <46450678+hensol1@users.noreply.github.com>
2024-10-07 23:10:12 +0300

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-   import AuthComponent from './components/AuthComponent';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import AuthComponent from './components/AuthComponent';
+import config from './config';
 
-   function App() {
-     return (
-       <div className="App">
-         <h1>We Know Better</h1>
-         <AuthComponent />
-       </div>
-     );
-   }
+function App() {
+  return (
+    <GoogleOAuthProvider clientId={config.googleClientId}>
+      <div className="App">
+        <h1>We Know Better</h1>
+        <AuthComponent />
+      </div>
+    </GoogleOAuthProvider>
+  );
+}
 
-   export default App;
+export default App;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || 'https://projectwe.onrender.com'
+  apiUrl: process.env.REACT_APP_API_URL || 'https://projectwe.onrender.com',
+  googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID
 };
 
 export default config;
