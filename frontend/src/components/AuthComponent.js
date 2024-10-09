076cca5c7bd5ad6cd9488daf8be418447126c110
hensol1 <46450678+hensol1@users.noreply.github.com>
2024-10-09 09:28:58 +0300

    Update AuthComponent.js

--- frontend/src/components/AuthComponent.js
+++ frontend/src/components/AuthComponent.js
@@ -168,7 +168,7 @@ const AuthComponent = () => {
         <>
           <button
             onClick={() => setIsModalOpen(true)}
-             className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600"
+             className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Sign In
           </button>
