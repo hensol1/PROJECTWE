67d982c3b88beb5889fac746477c7736fe7b17b1
hensol1 <46450678+hensol1@users.noreply.github.com>
2024-12-17 14:47:09 +0200

    Update fetchMatches.js

--- backend/fetchMatches.js
+++ backend/fetchMatches.js
@@ -8,7 +8,7 @@ const BASE_URL = "https://v3.football.api-sports.io";
 const HEADERS = { "x-rapidapi-key": API_KEY, "x-rapidapi-host": "v3.football.api-sports.io" };
 
 // MongoDB Configuration
-const MONGO_URI = "mongodb://localhost:27017/";
+const MONGO_URI = "mongodb+srv://weknowbetteradmin:dMMZV14rCKTYLJXG@cluster0.sbr1j.mongodb.net/";
 const DB_NAME = "test";
 const COLLECTION_NAME = "matches";
 
